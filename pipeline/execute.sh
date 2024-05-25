65dff0e952804e5b92ad40a1ca4a9b6633f1ccab
r00ta <jacopo.r00ta@gmail.com>
2024-05-26 00:37:31 +0200

    revert arguments

--- pipeline/execute.sh
+++ pipeline/execute.sh
@@ -4,31 +4,29 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 . $SCRIPT_DIR/../utils/functions.sh
 
+# Initialize a variable to hold the value of --maas_dir
 maas_dir=""
-random=""
 
-# Parse the command line arguments
-while [[ $# -gt 0 ]]
-do
-    case "$1" in
+# Parse the arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
         --maas_dir)
             maas_dir="$2"
-            shift # past argument
-            shift # past value
-            ;;
-        --random)
-            random="$2"
-            shift # past argument
-            shift # past value
+            shift 2
             ;;
         *)
-            echo "Unknown option $1"
+            echo "Unknown parameter passed: $1"
             exit 1
             ;;
     esac
 done
 
-export SUBNET_PREFIX="$random"
+# Check if the --maas_dir argument was provided
+if [ -z "$maas_dir" ]; then
+    echo "Usage: $0 --maas_dir <directory>"
+    exit 1
+fi
+
 export MAC_1=$(get_mac)
 export MAC_2=$(get_mac)
 export MAAS_DIR="$maas_dir"
