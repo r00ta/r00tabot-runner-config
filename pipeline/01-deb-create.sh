3e35517c4766a85fc989de79029663183f3677dc
r00ta <jacopo.r00ta@gmail.com>
2024-05-26 14:37:34 +0200

    remove useless logs

--- pipeline/01-deb-create.sh
+++ pipeline/01-deb-create.sh
@@ -1,9 +1,11 @@
+set -x 
+
 lxc launch ubuntu:22.04 $CONTAINER_NAME
 
 # wait for container
 sleep 10 
 
-lxc file push -r $MAAS_DIR $CONTAINER_NAME/home/ubuntu/
+lxc file push -r $MAAS_DIR $CONTAINER_NAME/home/ubuntu/ > /dev/null
 lxc exec $CONTAINER_NAME --cwd /home/ubuntu/ --user 0 -- apt-get update 
 lxc exec $CONTAINER_NAME --cwd /home/ubuntu/maas --user 1000 -- git submodule update --init --recursive
 lxc exec $CONTAINER_NAME --cwd /home/ubuntu/maas --user 0 -- apt-get install make
@@ -11,11 +13,15 @@ lxc exec $CONTAINER_NAME --cwd /home/ubuntu/maas --user 0 -- make install-depend
 lxc exec $CONTAINER_NAME --cwd /home/ubuntu/maas --user 1000 -- make package
 
 TMPDIR=$(mktemp -d)
-lxc file pull -r $CONTAINER_NAME/home/ubuntu/build-area/ $TMPDIR
+lxc file pull -r $CONTAINER_NAME/home/ubuntu/build-area/ $TMPDIR > /dev/null
 lxc delete -f $CONTAINER_NAME
+
+sleep 5
 
 lxc launch ubuntu:22.04 $CONTAINER_NAME
 lxc config device add $CONTAINER_NAME eth1 nic name=eth1 nictype=bridged parent=$CONTAINER_NAME
+
+sleep 10
 
 lxc exec $CONTAINER_NAME --user 0 -- sh -c "echo 'Acquire::http::Proxy \"http://172.0.2.15:3129\";' | sudo tee /etc/apt/apt.conf.d/99proxy"
 
@@ -29,7 +35,7 @@ network:
 \" >> /etc/netplan/99-static-eth1.yaml"
 lxc exec $CONTAINER_NAME --user 0 -- netplan apply 
 
-lxc file push -r $TMPDIR/build-area $CONTAINER_NAME/home/ubuntu
+lxc file push -r $TMPDIR/build-area $CONTAINER_NAME/home/ubuntu > /dev/null
 rm -rf $TMPDIR
 lxc exec $CONTAINER_NAME --user 0 --  add-apt-repository -y ppa:maas-committers/latest-deps
 lxc exec $CONTAINER_NAME --user 0 -- apt-get update
