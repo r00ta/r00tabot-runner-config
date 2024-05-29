96b7e7f82567c6dce94e3746c1022abe1aacdf5d
Jacopo Rota <jacopo.r00ta@gmail.com>
2024-05-29 15:19:10 +0200

    Update 01-snap-create.sh

--- pipeline/01-snap-create.sh
+++ pipeline/01-snap-create.sh
@@ -10,17 +10,6 @@ git -C $MAAS_DIR submodule update --init --recursive
 (cd $MAAS_DIR && make snap-tree)
 
 lxc exec $CONTAINER_NAME --user 0 -- sh -c "echo 'Acquire::http::Proxy \"http://172.0.2.15:3129\";' | sudo tee /etc/apt/apt.conf.d/99proxy"
-lxc file push $SCRIPT_DIR/environment $CONTAINER_NAME/tmp/
-lxc exec $CONTAINER_NAME --user 0 --cwd /tmp/ -- sh -c "
-cat << EOF >> /etc/environment
-    http_proxy=\"http://172.0.2.15:3129/\"
-    https_proxy=\"http://172.0.2.15:3129/\"
-    no_proxy=\"localhost,127.0.0.1,localaddress,.localdomain.com,$SUBNET_PREFIX.0.1.2\"
-    HTTP_PROXY=\"http://172.0.2.15:3129/\"
-    HTTPS_PROXY=\"http://172.0.2.15:3129/\"
-    NO_PROXY=\"localhost,127.0.0.1,localaddress,.localdomain.com,$SUBNET_PREFIX.0.1.2\"
-EOF
-"
 
 lxc exec $CONTAINER_NAME --user 0 --cwd /home/ubuntu/ -- sh -c "printf \"
 network:
