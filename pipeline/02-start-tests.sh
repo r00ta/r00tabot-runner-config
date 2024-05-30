70130c3f0398165d7818b3d2c2cf6dc77b3b6947
Jacopo Rota <jacopo.r00ta@gmail.com>
2024-05-30 12:10:53 +0200

    Update 02-start-tests.sh

--- pipeline/02-start-tests.sh
+++ pipeline/02-start-tests.sh
@@ -1,16 +1,31 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+exec_and_check() {
+  lxc exec $CONTAINER_NAME --user 1000 --cwd /home/ubuntu/tests -- sh -c "$1"
+  if [ $? -ne 0 ]; then
+    echo "Command '$1' failed"
+    exit 1
+  fi
+}
+
+
 lxc exec $CONTAINER_NAME --user 0 -- snap install jq --classic
 lxc file push -r $SCRIPT_DIR/../tests $CONTAINER_NAME/home/ubuntu
 lxc exec $CONTAINER_NAME --cwd /home/ubuntu --user 0 -- chown -R ubuntu:ubuntu tests
 lxc exec $CONTAINER_NAME --user 0 --cwd /home/ubuntu/ -- chmod +x -R /home/ubuntu/tests
 lxc exec $CONTAINER_NAME --user 0 --cwd /tmp/ -- sh -c "printf \"\$(maas apikey --username maas)\" > api-key-file"
 
-lxc exec $CONTAINER_NAME --user 1000 --cwd /home/ubuntu/tests -- sh -c "SUBNET_PREFIX=$SUBNET_PREFIX ./00-setup.sh" 
+# Execute the commands and check for failures
+exec_and_check "SUBNET_PREFIX=$SUBNET_PREFIX ./00-setup.sh"
 lxc start vm01-$SUBNET_PREFIX
-lxc exec $CONTAINER_NAME --user 1000 --cwd /home/ubuntu/tests -- sh -c "SUBNET_PREFIX=$SUBNET_PREFIX ./01-enlist.sh" 
-lxc exec $CONTAINER_NAME --user 1000 --cwd /home/ubuntu/tests -- sh -c "./02-commission.sh" 
-lxc exec $CONTAINER_NAME --user 1000 --cwd /home/ubuntu/tests -- sh -c "./03-deploy.sh" 
-lxc exec $CONTAINER_NAME --user 1000 --cwd /home/ubuntu/tests -- sh -c "./04-ssh.sh" 
-lxc exec $CONTAINER_NAME --user 1000 --cwd /home/ubuntu/tests -- sh -c "./05-release.sh" 
+if [ $? -ne 0 ]; then
+    echo "Command 'lxc start vm01-$SUBNET_PREFIX' failed"
+    exit 1
+fi
+exec_and_check "SUBNET_PREFIX=$SUBNET_PREFIX ./01-enlist.sh"
+exec_and_check "./02-commission.sh"
+exec_and_check "./03-deploy.sh"
+exec_and_check "./04-ssh.sh"
+exec_and_check "./05-release.sh"
+
 
