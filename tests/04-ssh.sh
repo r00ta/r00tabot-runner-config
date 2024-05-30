25b4703fbc79bcd3c135ec66b9c964f032b822dd
Jacopo Rota <jacopo.r00ta@gmail.com>
2024-05-30 22:57:37 +0200

    Update 04-ssh.sh

--- tests/04-ssh.sh
+++ tests/04-ssh.sh
@@ -19,15 +19,15 @@ retry_limit=300
 retry_interval=10
 
 while [ $retry_count -lt $retry_limit ]; do
-    ssh_result=$(ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@$IP_ADDRESS -i /tmp/id_rsa id 2>/dev/null)
+    ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@$IP_ADDRESS -i /tmp/id_rsa id | grep ubuntu
 
-    if [[ "$ssh_result" == *"ubuntu"*  ]]; then
-        echo "SSH login successful. User is 'ubuntu'."
-        exit 0
-    else
+    if [ $? -ne 0 ]; then 
         echo "SSH login failed or user is not 'ubuntu'. Retrying..."
         sleep $retry_interval
         retry_count=$((retry_count + retry_interval))
+    else
+        echo "SSH login successful. User is 'ubuntu'."
+        exit 0
     fi
 done
 
