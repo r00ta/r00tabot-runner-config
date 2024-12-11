503260752b7838094ac3d85ddb43687d4794470a
Jacopo Rota <jacopo.r00ta@gmail.com>
2024-12-11 09:40:50 +0100

    Update 01-snap-create.sh

--- pipeline/01-snap-create.sh
+++ pipeline/01-snap-create.sh
@@ -34,7 +34,7 @@ lxc exec $CONTAINER_NAME --user 0 --cwd /home/ubuntu/ -- ./connect-snap-interfac
 lxc exec $CONTAINER_NAME --user 0 --cwd /home/ubuntu -- apt-get install postgresql -y 
 lxc exec $CONTAINER_NAME --user 113 -- psql -c "CREATE USER \"maasdb\" WITH ENCRYPTED PASSWORD 'maasdb'"
 lxc exec $CONTAINER_NAME --user 113 -- createdb -O "maasdb" "maasdb" 
-lxc exec $CONTAINER_NAME --user 0 --cwd /home/ubuntu -- sh -c "echo 'host    maasdb   maasdb    0/0     md5' >> /etc/postgresql/14/main/pg_hba.conf"
+lxc exec $CONTAINER_NAME --user 0 --cwd /home/ubuntu -- sh -c "echo 'host    maasdb   maasdb    0/0     md5' >> /etc/postgresql/16/main/pg_hba.conf"
 lxc exec $CONTAINER_NAME --user 0 --cwd /home/ubuntu -- systemctl restart postgresql 
 lxc exec $CONTAINER_NAME --user 0 --cwd /home/ubuntu -- sh -c "maas init region+rack --database-uri 'postgres://maasdb:maasdb@localhost/maasdb'  --maas-url http://$SUBNET_PREFIX.0.1.2:5240/MAAS"
 lxc exec $CONTAINER_NAME --user 0 --cwd /home/ubuntu -- maas createadmin --username maas --password maas --email maas 
