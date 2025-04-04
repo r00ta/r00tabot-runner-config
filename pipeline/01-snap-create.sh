c4bffcbcfe2742b4fbc83b9293df6cc7901ad9d2
Jacopo Rota <jacopo.rota@canonical.com>
2025-04-04 09:39:44 +0200

    fix tokens

--- pipeline/01-snap-create.sh
+++ pipeline/01-snap-create.sh
@@ -3,10 +3,22 @@
 cd $MAAS_DIR
 
 make snap-tree
+sudo snap install maas-test-db
 sudo snap install --dangerous dev-snap/maas.snap
 utilities/connect-snap-interfaces
 
-sudo snap install maas-test-db
 
 sudo maas init region+rack --database-uri maas-test-db:///  --maas-url http://10.0.1.1:5240/MAAS
 sudo maas createadmin --username maas --password maas --email maas
+
+printf "Waiting for MAAS to be up and running.."
+while true; do
+    status=$(curl -s -o /dev/null -w "%{http_code}" http://10.0.1.1:5240/MAAS/a/openapi.json)
+    if [ "$status" -eq 200 ]; then
+        echo "Server is up! (HTTP 200)"
+        break
+    else
+        echo "Waiting... (HTTP $status)"
+        sleep 2  # Wait 1 second before retrying
+    fi
+done
