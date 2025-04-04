8f71235268811df531f798e7cd8479fa650a53b9
Jacopo Rota <jacopo.r00ta@gmail.com>
2025-04-04 17:49:37 +0200

    Update 00-setup-machine.sh

--- pipeline/00-setup-machine.sh
+++ pipeline/00-setup-machine.sh
@@ -9,6 +9,7 @@ sudo snap install jq --classic
 echo "Installing LXD.."
 sudo snap install --channel=latest/stable lxd
 sudo adduser runner lxd
+su - runner
 
 echo "Configuring LXD.."
 cat <<EOF | sudo lxd init --preseed
