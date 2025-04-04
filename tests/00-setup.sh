c65079b281c5f579b2eb8fcdf259efe7a289b73f
Jacopo Rota <jacopo.r00ta@gmail.com>
2025-04-05 00:27:32 +0200

    Update 00-setup.sh

--- tests/00-setup.sh
+++ tests/00-setup.sh
@@ -29,8 +29,8 @@ lxc config trust add --name maas -q > /tmp/lxd-token
 
 # MAAS might be slow at startup and return 502 here.
 maas login admin http://localhost:5240/MAAS `cat /tmp/api-key-file`
-maas admin maas set-config name="enable_http_proxy" value=True
-maas admin maas set-config name="http_proxy" value="http://172.0.2.17:8000"
+maas admin maas set-config name=enable_http_proxy value=True
+maas admin maas set-config name=http_proxy value=http://172.0.2.17:8000
 
 # Use local mirror
 maas admin boot-resources stop-import
