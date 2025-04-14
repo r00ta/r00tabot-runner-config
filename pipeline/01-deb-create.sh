f35e8fd46332b8c75f79eed54b09ec3a08e5876a
Jacopo Rota <jacopo.r00ta@gmail.com>
2025-04-14 14:55:06 +0200

    Update 01-deb-create.sh

--- pipeline/01-deb-create.sh
+++ pipeline/01-deb-create.sh
@@ -13,5 +13,5 @@ echo "Installing MAAS.."
 sudo add-apt-repository -y ppa:maas-committers/latest-deps
 sudo apt-get update
 sudo apt install -y ../build-area/*.deb
-maas createadmin --username maas --password maas --email maas
+sudo maas createadmin --username maas --password maas --email maas
 
