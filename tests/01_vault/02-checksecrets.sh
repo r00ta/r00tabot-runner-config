87712856ce6a95d8050d3c5180cb8e22df0f0fba
Jacopo Rota <jacopo.rota@canonical.com>
2025-04-18 21:53:22 +0200

    retry login

--- tests/01_vault/02-checksecrets.sh
+++ tests/01_vault/02-checksecrets.sh
@@ -3,7 +3,7 @@
 set -ex
 
 echo "Logging in.."
-maas login admin http://localhost:5240/MAAS `cat /tmp/api-key-file`
+retry_until_success_contains "maas login admin http://localhost:5240/MAAS `cat /tmp/api-key-file`" "You are now logged in to the MAAS server at"
 
 echo "Setting maas_auto_ipmi_k_g_bmc_key configuration.."
 maas admin maas set-config name=maas_auto_ipmi_k_g_bmc_key value=0x0000000000000000000000000000000000000000
