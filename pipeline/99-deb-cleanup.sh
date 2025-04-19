c799e6c31323cdbfe0d51db9718f075b25bc2676
Jacopo Rota <jacopo.r00ta@gmail.com>
2025-04-19 11:08:30 +0200

    Update 99-deb-cleanup.sh

--- pipeline/99-deb-cleanup.sh
+++ pipeline/99-deb-cleanup.sh
@@ -2,7 +2,4 @@ set -ex
 
 
 echo "Cleaning up.."
-sudo apt-get purge maas ; sudo apt-get autoremove
-
-echo "Cleaning db.."
-sudo -u postgres psql -c "DROP DATABASE maas;"
+sudo DEBIAN_FRONTEND=noninteractive apt remove --purge -y 'maas*' 'postgresql*' && sudo DEBIAN_FRONTEND=noninteractive apt -y autoremove
